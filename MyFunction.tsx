f80873f119e5e656bdcd055696b44426a6f8c84e
Yasnovsky <andrey5.1997@mail.ru>
2023-05-05 19:40:10 +0500

    commit8

---
+++ MyFunction.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyFunction({name, price,}) {
+  return (
+    <div>
+        <span>{name}</span>
+        <span>{price}</span>
+    </div>
+  )
+}
+
+export default MyFunction
