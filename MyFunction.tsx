8f00e233ce9b52c7d6b4c22d7989b0de109d4b42
Yasnovsky <andrey5.1997@mail.ru>
2023-05-05 10:20:40 +0500

    commit4

--- MyFunction.tsx
+++ MyFunction.tsx
@@ -1,17 +1,20 @@
 type MyFunctionType = {
     name: string;
     price: number;
+    sale?: number;
 }
 
 function MyFunction({ 
   name, 
   price,
+  sale,
 }: MyFunctionType) {
-  
+
   return (
     <>
       <div>{name}</div>
       <div>{price}</div>
+      <div>{sale}</div>
     </>
   )
 }
